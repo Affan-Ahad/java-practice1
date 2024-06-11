f297a241ec7777a45eccd3be611b94209fd15443
Affan-Ahad <affanahad870@gmail.com>
2024-06-10 20:17:13 -0700

    Added details about myself

---
+++ AffanDetails.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class AffanDetails {
+public static void main(String arg[]) {
+
+System.out.print("My name is Affan Ahad\n");
+
+System.out.println ("I am  a tech lover,\t who like to learn new things");
+
+System.out.println ("This is a sample of basic coding which I am doing");
+	}
+}	
