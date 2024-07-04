28012e6bdb733163d9f583c35a1c4597b5743c1e
Affan-Ahad <affanahad870@gmail.com>
2024-07-03 20:46:38 -0700

    Java basic codes

---
+++ ConstructorSample.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class ConstructorSample {
+ 
+  public ConstructorSample(String firstName, String lastName) {
+   firstName= "Affan";
+   lastName="Abdul";
+  }
+  
+   public ConstructorSample(String address) {
+   address= "USA";
+   }
+   
+   public static void main (String arg[]) {
+   System.out.println ("Example of method");
+  
+} 
---
+++ LoopArrey.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+ class LoopArrey {
+	  public static void main(String arg[]) throws InterruptedException { 
+	for(int i=10; i<=50; i++) {
+		 System.out.print(i+", ");
+		 Thread.sleep(5000);
+		}
+	  }
+ }
+		//Dry run: 
+		//i=10; i<= 50: true; 1  10
+		//i=11; i
